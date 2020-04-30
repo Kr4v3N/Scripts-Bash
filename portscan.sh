af0da5cf87badb5b4bbfcc549576a6c09e15b7b2
Kr4v3n <fafachen37@gmail.com>
2020-04-30 08:47:41 +0200

    Add portscan

---
+++ portscan.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# A simple bash script to grab open ports with nmap, then scan only those open ports more aggressively
+# Example : ./portscan 192.168.0.1 -oX nmap.xml
+#
+echo Grabbing ports...
+
+ports=$(nmap -p- --min-rate 1000 -T4 $1 | grep ^[0-9] | cut -d '/' -f 1 | tr '\n' ',' | sed s/,$//)
+
+echo Ports grabbed!
+
+echo Scanning...
+
+nmap -sC -sV -Pn -p $ports $1 $2 $3
